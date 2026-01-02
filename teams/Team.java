651b33a973c9a9f68811407c30d4868c01a9abff
chalkinshmeal <cmeyers184@gmail.com>
2026-01-01 20:32:49 -0600

    Refining TeamHandler

--- teams/Team.java
+++ teams/Team.java
@@ -1,11 +1,11 @@
 package chalkinshmeal.mc_plugin_lib.teams;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
@@ -17,34 +17,66 @@ import net.kyori.adventure.text.Component;
 // NOT the same as Spigot API's Scoreboard.Team
 //-----------------------------------------------------------------------------
 public class Team {
-    private final String name;
+    private final String key;
     private Component displayName;
-    private final Set<UUID> players = new HashSet<>();
+    private Material material;
+    private final Set<UUID> playerUUIDs = new HashSet<>();
 
-    public Team(String name, Component displayName) {
-        this.name = name;
+    public Team(String key, Component displayName) {
+        this.key = key;
         this.displayName = displayName;
+    }
+
+    public Team(String key, Component displayName, Material material) {
+        this.key = key;
+        this.displayName = displayName;
+        this.material = material;
     }
 
     //-------------------------------------------------------------------------
     // Accessor/Mutators
     //-------------------------------------------------------------------------
-    public String getName() { return this.name; }
+    public String getKey() { return this.key; }
     public Component getDisplayName() { return this.displayName; }
+    public Material getMaterial() { return this.material; }
 
     //-------------------------------------------------------------------------
     // Player methods
     //-------------------------------------------------------------------------
-    public void addPlayer(Player player) { players.add(player.getUniqueId()); }
-    public void removePlayer(Player player) { players.remove(player.getUniqueId()); }
-    public boolean hasPlayer(Player player) { return players.contains(player.getUniqueId()); }
-    public Set<UUID> getUUIDs() { return Collections.unmodifiableSet(players); }
-    public Set<Player> getOnlinePlayers() {
-        Set<Player> players = new HashSet<>();
+    public void addPlayer(Player player) { playerUUIDs.add(player.getUniqueId()); }
+    public void removePlayer(Player player) { playerUUIDs.remove(player.getUniqueId()); }
+    public boolean hasPlayer(Player player) { return playerUUIDs.contains(player.getUniqueId()); }
+    public Set<UUID> getUUIDs() { return this.playerUUIDs; }
+    public Set<UUID> getOnlineUUIDs() {
+        Set<UUID> onlineUUIDs = new HashSet<>();
         for (UUID uuid : this.getUUIDs()) {
             OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+            if (offlinePlayer.isOnline()) onlineUUIDs.add(uuid);
+        }
+        return onlineUUIDs;
+    }
+
+    public Set<OfflinePlayer> getPlayers() {
+        Set<OfflinePlayer> players = new HashSet<>();
+        for (UUID uuid : this.getUUIDs()) players.add(Bukkit.getOfflinePlayer(uuid));
+        return players;
+    }
+    public Set<Player> getOnlinePlayers() {
+        Set<Player> players = new HashSet<>();
+        for (OfflinePlayer offlinePlayer : this.getPlayers()) {
             if (offlinePlayer.isOnline()) players.add(offlinePlayer.getPlayer());
         }
         return players;
+    }
+    public int getNumPlayers() { return this.playerUUIDs.size(); }
+    public Set<String> getPlayerNames() {
+        Set<String> playerNames = new HashSet<>();
+        for (UUID uuid : this.getUUIDs()) {
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+            String playerName = offlinePlayer.getName();
+            playerNames.add(playerName);
+            //playerNames.add(Bukkit.getPlayer(uuid).getName());
+        }
+        return playerNames;
     }
 }
--- teams/TeamHandler.java
+++ teams/TeamHandler.java
@@ -2,10 +2,15 @@ package chalkinshmeal.mc_plugin_lib.teams;
 
 import static chalkinshmeal.mc_plugin_lib.logging.LoggerUtils.warn;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -27,76 +32,131 @@ public class TeamHandler {
     private final ScoreboardHandler scoreboardHandler;
     private final Map<String, Team> teams = new HashMap<>();
     private final Set<Listener> listeners = Set.of(new PlayerJoinListener(), new PlayerQuitListener());
+    private boolean isScoreboardVisible = false;
 
     public TeamHandler(JavaPlugin plugin, String scoreboardTitle) {
         this.plugin = plugin;
         this.scoreboardHandler = new ScoreboardHandler();
         this.scoreboardHandler.setTitle(scoreboardTitle);
     }
+
+    //-------------------------------------------------------------------------
+    // Accessor/Mutators
+    //-------------------------------------------------------------------------
+    public void setScoreboardVisible(boolean isVisible) { this.isScoreboardVisible = isVisible; }
+    public boolean isScoreboardVisible() { return this.isScoreboardVisible; }
     
     //-------------------------------------------------------------------------
     // Team methods
     //-------------------------------------------------------------------------
     public Set<String> getTeamNames() { return this.teams.keySet(); }
-    public Set<Team> getTeams() { return (Set<Team>) this.teams.values(); }
+    public Set<Team> getTeams() { return new HashSet<>(this.teams.values()); }
     public int getNumTeams() { return this.teams.size(); }
-    public boolean hasTeam(String teamName) { return this.teams.containsKey(teamName); }
+    public Team getTeam(String teamKey) { return this.teams.get(teamKey); }
+    public boolean hasTeam(String teamKey) { return this.teams.containsKey(teamKey); }
+    public boolean hasTeam(Team team) { return this.teams.containsKey(team.getKey()); }
+    public boolean hasTeam(Player player) {
+        for (Team team : this.getTeams()) if (team.hasPlayer(player)) return true;
+        return false;
+    }
 
-    public void addTeam(String teamName, Component displayName, int startingScore) {
-        if (this.hasTeam(teamName)) throw new IllegalArgumentException("Team already exists: '" + teamName + "'");
-        Team team = new Team(teamName, displayName);
-        this.teams.put(teamName, team);
+    public void addTeam(String teamKey, Component displayName, Material material, int startingScore) {
+        if (this.hasTeam(teamKey)) throw new IllegalArgumentException("Team already exists: '" + teamKey + "'");
+        Team team = new Team(teamKey, displayName, material);
+        this.teams.put(teamKey, team);
         this.scoreboardHandler.addEntry(team.getDisplayName(), startingScore);
     }
 
-    public void removeTeamIfExists(String teamName) {
-        if (!this.hasTeam(teamName)) { warn("Team does not exist: '" + teamName + "'"); return; }
-        this.teams.remove(teamName);
-        this.scoreboardHandler.removeEntryIfExists(teamName);
+    public void removeTeamIfExists(String teamKey) {
+        if (!this.hasTeam(teamKey)) { warn("Team does not exist: '" + teamKey + "'"); return; }
+        this.teams.remove(teamKey);
+        this.scoreboardHandler.removeEntryIfExists(teamKey);
     }
 
     public void removeEmptyTeams() {
-        for (String teamName : this.teams.keySet()) {
-            if (this.getNumPlayers(teamName) == 0) this.removeTeamIfExists(teamName);
+        for (String teamKey : new ArrayList<>(this.getTeamNames())) {
+            Team team = this.getTeam(teamKey);
+            if (team.getNumPlayers() == 0) this.removeTeamIfExists(teamKey);
         }
+    }
+
+    public Team getTeam(Player player) {
+        if (!this.getAllPlayers().contains(player)) throw new IllegalArgumentException("Player not part of a team: '" + player.getName() + "'");
+        for (Team team : this.getTeams()) if (team.hasPlayer(player)) return team;
+        return null;
     }
 
     public Set<Team> getWinningTeams() {
         int maxScore = this.getMaxScore();
-        Set<Team> winningTeams = Set.of();
+        Set<Team> winningTeams = new HashSet<>();
         for (Team team : this.getTeams()) {
             if (this.getScore(team) == maxScore) winningTeams.add(team);
         }
         return winningTeams;
     }
 
+    public Set<Team> getTeamsWithPositiveLives() {
+        Set<Team> positiveTeams = new HashSet<>();
+        for (Team team : this.getTeams()) if (this.getScore(team) > 0) positiveTeams.add(team);
+        return positiveTeams;
+    }
+
+    public Set<Team> getLosingTeams() {
+        int maxScore = this.getMaxScore();
+        Set<Team> losingTeams = new HashSet<>();
+        for (Team team : this.getTeams()) {
+            if (this.getScore(team) < maxScore) losingTeams.add(team);
+        }
+        return losingTeams;
+    }
+
     //-------------------------------------------------------------------------
     // Player methods
     //-------------------------------------------------------------------------
-    public int getNumPlayers(String teamName) { return this.getPlayers(teamName).size(); }
-
-    public void addPlayer(Player player, String teamName) {
-        if (!this.hasTeam(teamName)) throw new IllegalArgumentException("Team does not exist: '" + teamName + "'");
-        Team team = this.teams.get(teamName);
+    public void addPlayer(Player player, String teamKey) {
+        if (!this.hasTeam(teamKey)) throw new IllegalArgumentException("Team does not exist: '" + teamKey + "'");
+        Team team = this.teams.get(teamKey);
         team.addPlayer(player);
-        this.scoreboardHandler.showToPlayer(player);
     }
 
-    public void removePlayer(Player player, String teamName) {
-        if (!this.hasTeam(teamName)) throw new IllegalArgumentException("Team does not exist: '" + teamName + "'");
-        Team team = this.teams.get(teamName);
+    public void addPlayer(Player player, int teamIndex) {
+        if (teamIndex + 1 > this.getNumTeams()) throw new IllegalArgumentException("Attempted to add player to out-of-bounds team index: '" + teamIndex + "'");
+        Team team = new ArrayList<>(this.getTeams()).get(teamIndex);
+        team.addPlayer(player);
+    }
+
+    public void removePlayerIfExists(Player player) {
+        if (!this.hasTeam(player)) return;
+        Team team = this.getTeam(player);
         team.removePlayer(player);
-        this.scoreboardHandler.hideFromPlayer(player);
     }
 
-    public Set<Player> getPlayers(String teamName) {
-        if (!this.hasTeam(teamName)) throw new IllegalArgumentException("Team does not exist: '" + teamName + "'");
-        return this.teams.get(teamName).getOnlinePlayers();
+    public Set<Player> getPlayers(String teamKey) {
+        if (!this.hasTeam(teamKey)) throw new IllegalArgumentException("Team does not exist: '" + teamKey + "'");
+        return this.teams.get(teamKey).getOnlinePlayers();
     }
 
-    public Set<Player> getAllPlayers() {
-        Set<Player> players = Set.of();
+    public Set<OfflinePlayer> getAllPlayers() {
+        Set<OfflinePlayer> players = new HashSet<>();
+        for (Team team : this.getTeams()) players.addAll(team.getPlayers());
+        return players;
+    }
+
+    public Set<Player> getAllOnlinePlayers() {
+        Set<Player> players = new HashSet<>();
         for (Team team : this.getTeams()) players.addAll(team.getOnlinePlayers());
+        return players;
+    }
+
+    public Set<UUID> getAllOnlineUUIDs() {
+        Set<UUID> uuids = new HashSet<>();
+        for (Team team : this.getTeams()) uuids.addAll(team.getOnlineUUIDs());
+        return uuids;
+    }
+
+    public Set<Player> getLosingPlayers() {
+        Set<Player> players = new HashSet<>();
+        for (Team team : this.getLosingTeams()) players.addAll(team.getOnlinePlayers());
         return players;
     }
 
@@ -106,19 +166,14 @@ public class TeamHandler {
     public void addScore(Team team, int delta) { this.setScore(team, this.getScore(team) + delta); }
     public void subtractScore(Team team, int delta) { this.addScore(team, -delta); }
     public void setScore(Team team, int score) {
-        if (!this.hasTeam(team.getName())) throw new IllegalArgumentException("Team does not exist: '" + team.getName() + "'");
-        this.scoreboardHandler.setScore(team.getName(), score);
+        if (!this.hasTeam(team.getKey())) throw new IllegalArgumentException("Team does not exist: '" + team.getKey() + "'");
+        this.scoreboardHandler.setScore(team.getKey(), score);
     }
 
-    public int getScore(Team team) { return this.getScore(team.getName()); }
-    public int getScore(String teamName) {
-        if (!this.hasTeam(teamName)) throw new IllegalArgumentException("Team does not exist: '" + teamName + "'");
-        return this.scoreboardHandler.getScore(teamName);
-    }
-
-    public boolean noTeamHasPositiveLives() {
-        for (Team team : this.getTeams()) if (this.getScore(team) > 0) return false;
-        return true;
+    public int getScore(Team team) { return this.getScore(team.getKey()); }
+    public int getScore(String teamKey) {
+        if (!this.hasTeam(teamKey)) throw new IllegalArgumentException("Team does not exist: '" + teamKey + "'");
+        return this.scoreboardHandler.getScore(teamKey);
     }
 
     public int getMaxScore() {
@@ -128,9 +183,26 @@ public class TeamHandler {
         return maxScore;
     }
 
+    public boolean noTeamHasPositiveLives() { return getNumPositiveLivesTeams() == 0; }
+    public boolean atMostOneTeamHasPositiveLives() { return getNumPositiveLivesTeams() <= 1; }
+    public boolean atLeastOneTeamHasPositiveLives() { return getNumPositiveLivesTeams() >= 1; }
+    private int getNumPositiveLivesTeams() {
+        int positiveTeams = 0;
+        for (Team team : this.getTeams()) if (this.getScore(team) > 0) positiveTeams += 1;
+        return positiveTeams;
+    }
+
     //-------------------------------------------------------------------------
     // TeamHandler methods
     //-------------------------------------------------------------------------
+    public void showScoreboardToAllPlayers() {
+        for (Team team : teams.values()) {
+            for (Player player : team.getOnlinePlayers()) {
+                this.scoreboardHandler.showToPlayer(player);
+            }
+        }
+    }
+
     public void hideScoreboardFromPlayers() {
         for (Team team : teams.values()) {
             for (Player player : team.getOnlinePlayers()) {
@@ -163,6 +235,7 @@ public class TeamHandler {
     private class PlayerJoinListener implements Listener {
         @EventHandler
         public void onPlayerJoin(PlayerJoinEvent event) {
+            if (!hasTeam(event.getPlayer())) return;
             scoreboardHandler.showToPlayer(event.getPlayer());
         }
     }
